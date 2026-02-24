d989027683cf0cacd7c17198e98ead4ce442af5e
Valentin Agapitov <valentin.agapitov@exness.com>
2026-02-24 10:19:41 +0000

    Chapter 2 LangSmith

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ load_dotenv()
 PROVIDER = os.environ.get("PROVIDER")
 MODEL = os.environ.get("MODEL")
 API_KEY = os.environ.get("API_KEY")
+LANGSMITH_API_KEY = os.environ.get("LANGSMITH_API_KEY")
+
+# below should not be changed
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://eu.api.smith.langchain.com"
+# you can change this as preferred
+os.environ["LANGCHAIN_PROJECT"] = "langchain-course-langsmith"
